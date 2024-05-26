a55cee80b245012d16f43e4f40e866c9e268d086
Betty k <156065962+bettykgeorge@users.noreply.github.com>
2024-05-26 20:48:02 +0530

    Update betty.c

--- betty.c
+++ betty.c
@@ -5,14 +5,14 @@ int main()
     int A, B, sum = 0; 
   
     // Ask user to enter the two numbers 
-    printf("Enter two numbers A and B : \n"); 
+    printf("Enter three numbers A and B : \n"); 
   
     // Read two numbers from the user || A = 2, B = 3 
-    scanf("%d%d", &A, &B); 
+    scanf("%d%d", &A, &B, &c); 
   
     // Calculate the addition of A and B 
     // using '+' operator 
-    sum = A + B; 
+    sum = A + B + c; 
   
     // Print the sum 
     printf("add of A and B is: %d", sum); 
